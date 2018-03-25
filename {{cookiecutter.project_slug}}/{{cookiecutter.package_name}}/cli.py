c56359c6f625742eb18b705ede7b7dcb4bc63228
Antoine Cezar <antoine@cezar.fr>
2018-03-25 22:10:23 +0200

    Fix extraneous line in generated cli.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/cli.py
@@ -20,7 +20,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    {% if cookiecutter.exemple != 'n' -%}
+    {%- if cookiecutter.exemple != 'n' %}
     greet(args.name)
     {%- endif %}
 
